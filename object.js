1be9466654c3ab55e3d743aed90bf01b10f8526d
Kamrul Hasan <you@example.com>
2022-01-29 00:06:51 +0600

    Declare multiple objects with multiple properties

---
+++ object.js
@@ -0,0 +1,21 @@
+var student1 = {
+    id: 115, 
+    name: 'Kamrul Hasan',
+    marks: 81, 
+    phone: 017355
+};
+
+var mobile = {
+    color: 'black',
+    price: 343433,
+    screenSize: 5,
+    Storage: '16gb'
+};
+
+var book = {
+    title: 'Start with Why',
+    author: 'Simon Sinek',
+    price: 200,
+    pages: 246,
+    publisher: 'penguin'
+};
